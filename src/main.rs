a13350216b1b21c2723e6fa7599c0bf1c924890a
reebot <adhikaryreevu97@gmail.com>
2023-10-07 21:47:04 +0100

    Static HTML in the same folder for better clarity

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,13 @@
-// Import necessary modules and crates
-mod database;
-mod model;
-mod routes;
-mod schema;
-mod errors;
+use actix_web::{get, web, App, HttpServer, Responder};
 
-#[macro_use]
-extern crate diesel;
-extern crate dotenv;
-
-use actix_web::{web, App, HttpServer};
-use dotenv::dotenv;
-use std::env;
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Hello, world!"
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Load environment variables from .env file
-    dotenv().ok();
-
-    // Setup logging
-    env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-
-    // Initialize database connection pool
-    let pool = database::init_pool();
-
-    // Start HTTP server
-    HttpServer::new(move || {
-        App::new()
-            .data(pool.clone()) // Pass database pool to application
-            .service(
-                web::resource("/api/v1/bookings")
-                    .route(web::get().to(routes::bookings_route)),
-            )
-    })
+    HttpServer::new(|| App::new().service(hello))
         .bind("127.0.0.1:8080")?
         .run()
         .await
