a21de3e6394e9bafff5ea68bd7fec0933057cff6
reebot <adhikaryreevu97@gmail.com>
2023-10-08 22:04:02 +0100

    Revamped a lot of things in UI

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,106 @@
+use warp::Filter;
 use sqlx::postgres::PgPool;
 use std::env;
 use dotenv::dotenv;
 use chrono::{NaiveDate, NaiveTime};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use tokio;
+use sqlx::FromRow;
+use warp::filters::cors;
 
-#[derive(Debug, Deserialize)]
+
+#[derive(Debug, Deserialize, Serialize, FromRow)] // Ensure to derive FromRow
 pub struct Activity {
     pub id: String,
+    #[serde(rename = "activityType")] // Use serde rename if the field name doesn't match the column name
     pub activity_type: String,
     pub name: String,
     pub description: String,
+    #[serde(rename = "seatBooking")]
     pub seat_booking: String,
+    #[serde(rename = "waitingListCapacity")]
     pub waiting_list_capacity: i32,
+    #[serde(rename = "centerId")]
     pub center_id: i32,
+    #[serde(rename = "centerName")]
     pub center_name: String,
+    #[serde(rename = "personCenterId")]
     pub person_center_id: i32,
     pub date: NaiveDate,
+    #[serde(rename = "startTime")]
     pub start_time: NaiveTime,
+    #[serde(rename = "endTime")]
     pub end_time: NaiveTime,
+    #[serde(rename = "durationMinutes")]
     pub duration_minutes: i32,
 }
+async fn get_activities(pool: PgPool) -> Result<impl warp::Reply, warp::Rejection> {
+    let rows = sqlx::query_as::<_, Activity>(
+        r#"
+        SELECT * FROM activity
+        "#,
+    )
+        .fetch_all(&pool)
+        .await
+        .map_err(|e| {
+            eprintln!("db error: {}", e);
+            warp::reject::not_found()
+        })?;
+
+    Ok(warp::reply::json(&rows))
+}
+/*async fn get_activities(pool: PgPool) -> Result<impl warp::Reply, warp::Rejection> {
+    let date = "2023-10-08".parse::<NaiveDate>().unwrap();
+    let rows = sqlx::query_as::<_, Activity>(
+        r#"
+        SELECT * FROM activity WHERE date = $1
+        "#,
+    )
+        .bind(date)
+        .fetch_all(&pool)
+        .await
+        .map_err(|e| {
+            eprintln!("db error: {}", e);
+            warp::reject::not_found()
+        })?;
+
+    Ok(warp::reply::json(&rows))
+}*/
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PgPool::connect(&url).await?;
 
-    // Parse the string into a NaiveDate object
-    let date = "2023-10-08".parse::<NaiveDate>()?;
+    // Connect to the database and handle potential errors
+    let pool = match PgPool::connect(&url).await {
+        Ok(pool) => pool,
+        Err(e) => {
+            eprintln!("Error connecting to database: {}", e);
+            std::process::exit(1);
+        }
+    };
 
-    // Query to fetch all activities on a specific date
-    let rows = sqlx::query!(
-        r#"
-        SELECT * FROM activity WHERE date = $1
-        "#,
-        date
-    )
-        .fetch_all(&pool)
-        .await?;
+    let cors = cors::cors()
+        .allow_methods(vec!["GET", "POST", "DELETE"])
+        .allow_headers(vec!["Authorization", "Content-Type"])
+        .allow_any_origin();
 
-    for row in rows {
-        println!(
-            "Activity Name: {}, Description: {:?}, Start Time: {}",
-            row.name, row.description, row.start_time
-        );
-    }
+    let activity_route = warp::path("activities")
+        .and(warp::get())
+        .and(with_db(pool.clone()))
+        .and_then(get_activities);
+
+    // Use the `routes` variable here
+    let routes = activity_route.with(cors).with(warp::log("api"));
+
+    println!("Server started at http://127.0.0.1:8000"); // Log to know server has started
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 
     Ok(())
+}
+
+fn with_db(db_pool: PgPool) -> impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || db_pool.clone())
 }
--- static/script.js
+++ static/script.js
@@ -1,105 +1,86 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById('fetchButton').addEventListener('click', fetchData);
+    document.getElementById('fetchButton').addEventListener('click', fetchData);
 });
 
 async function fetchData() {
-	try {
-		const apiUrl = 'https://gymathonbe.radhikary.repl.co/api/bookings';
-		const response = await fetch(apiUrl);
-		const data = await response.json();
-		displayData(data);
-	} catch (error) {
-		console.error('Error fetching data: ', error);
-		alert('Failed to fetch data. Check console for error details.');
-	}
+    try {
+        const apiUrl = 'http://localhost:8000/activities';
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        displayData(data);
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+        alert('Failed to fetch data. Check console for error details.');
+    }
 }
 
 function displayData(data) {
-	const dataContainer = document.getElementById('dataContainer');
-	dataContainer.innerHTML = ''; // Clear previous data
+    const dataContainer = document.getElementById('dataContainer');
+    dataContainer.innerHTML = '';
 
-	// Create a table element
-	const table = document.createElement('table');
-	dataContainer.appendChild(table);
+    // Displaying Date below the Fetch Data Button
+    const dateLabel = document.createElement('p');
+    dateLabel.textContent = `Date: ${data[0].date}`;
+    dataContainer.appendChild(dateLabel);
 
-	// Add table headers
-	const headers = ['Name', 'Info', 'Starting Time'];
-	const thread = document.createElement('thread');
-	const headerRow = document.createElement('tr');
-	headers.forEach(header => {
-		const th = document.createElement('th');
-		th.textContent = header;
-		headerRow.appendChild(th);
-	});
-	thread.appendChild(headerRow);
-	table.appendChild(thread);
+    const table = document.createElement('table');
+    dataContainer.appendChild(table);
 
-	// Check if data is an array and not empty
-	if (Array.isArray(data) && data.length > 0) {
-		// Add table body
-		const body = document.createElement('body');
-		table.appendChild(body);
+    const headers = ['Name + Centre Name', 'Starting Time'];
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
 
-		// Iterate through data and add to table
-		data.forEach(booking => {
-			const row = document.createElement('tr');
+    if (Array.isArray(data) && data.length > 0) {
+        const tbody = document.createElement('tbody');
+        table.appendChild(tbody);
 
-			// Name column
-			const nameTd = document.createElement('td');
-			nameTd.textContent = booking.booking.name;
-			row.appendChild(nameTd);
+        data.forEach(activity => {
+            const row = document.createElement('tr');
 
-			// Info button column
-			const infoTd = document.createElement('td');
-			const infoButton = document.createElement('button');
-			infoButton.textContent = 'Info';
-			infoButton.onclick = () => showInfoPopup(booking.booking.description);
-			infoTd.appendChild(infoButton);
-			row.appendChild(infoTd)
+            // Name + Centre Name column
+            const nameTd = document.createElement('td');
+            nameTd.textContent = `${activity.name} (${activity.centerName})`; // Corrected property name
+            row.appendChild(nameTd);
 
-			// Starting time column
-			const timeTd = document.createElement('td');
-			const timeButton = document.createElement('button');
-			timeButton.textContent = booking.booking.startTime;
+            // Starting time column
+            const timeTd = document.createElement('td');
+            timeTd.textContent = activity.startTime; // Corrected property name
+            row.appendChild(timeTd);
 
-			// Check seat availability and set border color
-			if (booking.booking.bookedCount < booking.booking.classCapacity) {
-				timeButton.className = 'available';
-			} else {
-				timeButton.className = 'not-available';
-			}
-			timeTd.appendChild(timeButton);
-			row.appendChild(timeTd);
-
-			// Append the row to the body
-			body.appendChild(row);
-		});
-	} else {
-		// If no data, display a message
-		dataContainer.innerHTML = '<p>No bookings available.</p>';
-	}
+            tbody.appendChild(row);
+        });
+    } else {
+        dataContainer.innerHTML = '<p>No activities available.</p>';
+    }
 }
+
+function bookActivity(activityId) {
+    alert(`Booking activity with ID: ${activityId}`);
+}
+
 function showInfoPopup(description) {
-	// Create overlay
-	const overlay = document.createElement('div');
-	overlay.className = 'overlay';
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
 
-	// Create modal
-	const modal = document.createElement('div');
-	modal.className = 'modal';
+    const modal = document.createElement('div');
+    modal.className = 'modal';
 
-	// Create close button
-	const closeButton = document.createElement('button');
-	closeButton.textContent = 'Close';
-	closeButton.onclick = () => document.body.removeChild(overlay);
+    const closeButton = document.createElement('button');
+    closeButton.textContent = 'Close';
+    closeButton.onclick = () => document.body.removeChild(overlay);
 
-	// Create description paragraph
-	const descriptionP = document.createElement('p');
-	descriptionP.textContent = description;
+    const descriptionP = document.createElement('p');
+    descriptionP.textContent = description;
 
-	// Append elements
-	modal.appendChild(descriptionP);
-	modal.appendChild(closeButton);
-	overlay.appendChild(modal);
-	document.body.appendChild(overlay);
+    modal.appendChild(descriptionP);
+    modal.appendChild(closeButton);
+    overlay.appendChild(modal);
+    document.body.appendChild(overlay);
 }
